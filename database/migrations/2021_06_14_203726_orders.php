6c9dfead598e1280d1f68f19c8cc81ddaddbde9c
unknown <kwiatkowskimaciej1@gmail.com>
2021-06-14 22:43:50 +0200

    Orders table migration

---
+++ database/migrations/2021_06_14_203726_orders.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Orders extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('user');
+            $table->string('content');
+            $table->date('date_ordered');
+            $table->boolean('is_paid');
+            $table->string('payment_method');
+            $table->decimal('prize', 19, 4);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+}
